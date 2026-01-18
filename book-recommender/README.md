dd91d6c75264a37622d6439d1a75af9c63067c58
Sherifat-Aduku <sherifataduku.sa@gmail.com>
2026-01-18 08:05:09 +0100

    Added and styled book recommender using css properties

--- book-recommender/README.md
+++ book-recommender/README.md
@@ -5,4 +5,6 @@ This book recommender app was created using the following react hooks:
 - useEffect was used to create a side effect.
 - useReducer was used to manage multiple states, such as genre, mood and AI responses.
 - useCallback was used to remember the function to avoid rerendering/repetitions.
-- use gemini api for getting the response
+- used gemini api for getting the AI responses.
+
+This book app allow users to select a list of options from the drop down menu based on their preferred genre, mood, level and also ask AI to make recommendations.
--- book-recommender/src/App.jsx
+++ book-recommender/src/App.jsx
@@ -1,35 +1,125 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useReducer, useEffect, useCallback } from "react"
+import SelectField from "./components/Select.jsx"
+import listofMoodOption from "./store/mood.json"
+import listofGenreOption from "./store/genre.json"
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+const reducer = (state, action) => {
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  switch(action.type) {
+    case "Set_Genre":
+      return { ...state, genre: action.payload, mood: "" }
+  
+    case "Set_Mood":
+      return { ...state, mood: action.payload, }
+    
+    case "Set_Level":
+      return { ...state, level: action.payload, }
+    
+    case "Set_aiResponses":
+      return { ...state, aiResponses: action.payload || []}
+      default:
+      return state;
+  }
+};
 
-export default App
+export default function App() {
+
+  const [state, dispatch] = useReducer(reducer, {
+
+    genre: "",
+    mood: "",
+    level: "",
+    aiResponses: [] 
+    }
+   )
+
+   const availableMoodBasedOnGenre =
+    listofMoodOption[state.genre] || []
+
+   const fetchRecommendations = useCallback(async() => {
+
+    const { genre, mood, level } = state;
+
+    if (!genre || !mood || !level) return;
+
+     try {
+
+      const GEMINI_API_KEY = 'AIzaSyAaitFnIuiRbEjr7EcmMq9Ieg5LQJzAs2I';
+      const response = await fetch(
+        "https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=" +
+        GEMINI_API_KEY,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            contents: [{ parts: [{ text: `Recommend 6 books for a ${level} ${genre} reader feeling ${mood}. Explain why.` }] }]
+          })
+        }
+      )
+
+       const data = await response.json()
+
+    dispatch({
+      type: "Set_aiResponses",
+      payload: data?.candidates || []
+    })
+   } catch (err) {
+      console.log(err);
+    }
+  }, [state])
+  
+  useEffect(() => {
+    fetchRecommendations()
+  }, [fetchRecommendations])
+  
+  return(
+       <section>
+        <h1>Book Recommender</h1>
+       <SelectField
+        placeholder="Select a genre"
+        id="genre"
+        options={listofGenreOption}
+        value={state.genre}
+        onSelect={(value) =>
+        dispatch ({type: "Set_Genre" , payload: value})
+          }
+      />
+        
+       <SelectField
+        placeholder="Select a mood"
+        id="mood"
+        options={availableMoodBasedOnGenre}
+        value={state.mood}
+         onSelect={(value) =>
+        dispatch ({type: "Set_Mood" , payload: value})
+         }
+        />
+
+        <SelectField
+        placeholder="Select your level"
+        id="level"
+        options={["Beginner", "Intermediate", "Expert"]}
+        value={state.level}
+        onSelect={(value) =>
+        dispatch ({type: "Set_Level" , payload: value})
+         }
+        />
+
+      <br />
+         
+        <button onClick={fetchRecommendations}>Get Recommendation</button>
+
+      <br />
+      <br />
+    
+     {state.aiResponses.map((recommend, index) => (
+        <details key={index}>
+          <summary>Recommendation {index + 1}</summary>
+          <p>{recommend?.content?.[0]?.text}</p>
+        </details>
+      ))}
+      </section>
+     )
+      };
+  
